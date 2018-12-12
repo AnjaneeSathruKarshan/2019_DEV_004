994cb751d3227a59151659bc0702b2438eeac0ee
AnjaneeSathruKarshan <karshan.371@gmail.com>
2018-12-12 17:14:30 +0530

    App UI development

--- leapyear/src/App.js
+++ leapyear/src/App.js
@@ -2,27 +2,26 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+class LeapYear extends Component {
+  constructor(props) {
+    super(props);
+    this.textInput = React.createRef();
+    this.state = {
+            data: ''
+        }
+    }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className='App'>
+        <header><h1>Check if a year Leap year</h1></header>
+        <section>
+            <label>Enter a year</label><br/>
+            <input className='MarginTop10' ref={this.textInput} type='number'/><br/>
+            <button className='MarginTop10'>check</button>
+        </section>
       </div>
     );
   }
 }
 
-export default App;
+export default LeapYear;
