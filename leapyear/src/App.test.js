00eb6c3aeac8217b100816e414d9d499415c81df
AnjaneeSathruKarshan <karshan.371@gmail.com>
2018-12-12 18:43:52 +0530

    Added test dependency and test case for no input

--- leapyear/src/App.test.js
+++ leapyear/src/App.test.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Enzyme from 'enzyme';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import LeapYear from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+Enzyme.configure({ adapter: new Adapter() });
+
+test('check for no input', () => {
+  const App = Enzyme.mount(<LeapYear />);
+  App.find('input').instance().value = '';
+  App.find('button').simulate('click');
+  const warn = App.find('#warn');
+  expect(warn.text()).toEqual("Please enter year");
 });
